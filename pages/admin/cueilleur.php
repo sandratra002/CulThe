ee880c1978d3dd85875267b0c7a7c14e1816da2e
Sanda Silakiniaina <sandasilakiniaina4@gmail.com>
2024-02-13 00:14:16 +0300

    cleaning formulaire cueilleur

--- pages/admin/cueilleur.php
+++ pages/admin/cueilleur.php
@@ -31,21 +31,8 @@
         <section class="right">
             <form action="" method="post" id="login-form" class="form">
                 <h1 class="form__title">Gestion des employees</h1>
-
                 <hr class="form__sep">
-
                 <div class="form__content">
-                    <div class="form__input vertical">
-                        <label for="tea" class="form__label">Identification employee:</label>
-
-                        <select name="tea_id" id="tea" class="form__input-field">
-                            <option value="0">-</option>
-                            <option value="0">Emp 01</option>
-                            <option value="1">Emp 02</option>
-                            <option value="2">Emp 03</option>
-                        </select>
-                    </div>
-
                     <div class="form__input vertical">
                         <label for="nom" class="form__label">Nom:</label>
 
@@ -54,42 +41,19 @@
 
                     <div class="form__input-group horizontal">
                         <div class="form__input vertical">
-                            <label for="uid" class="form__label">Nom d'Identification:</label>
-
-                            <input type="number" name="uid" id="uid" class="form__input-field" />
-                        </div>
-                        <div class="form__input vertical">
                             <label for="birthday" class="form__label">Date de naissance:</label>
-
                             <input type="date" name="birthday" id="birthday" class="form__input-field" />
                         </div>
                     </div>
 
                     <div class="form__input-group horizontal">
                         <div class="form__input vertical">
-                            <label for="" class="form__label">Statut d'emploiement:</label>
-
-                            <div class="form__input-group horizontal">
-                                <div class="form__input horizontal">
-                                    <input type="radio" name="work_status" value="yes" id="yes"
-                                        class="form__input-field radio" checked>
-                                    <label for="yes">Oui</label>
-                                </div>
-                                <div class="form__input horizontal">
-                                    <input type="radio" name="work_status" value="no" id="no" class="form__input-field radio">
-                                    <label for="no">Non</label>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="form__input vertical">
                             <label for="salary" class="form__label">Salaire:</label>
 
                             <input type="number" name="salary" id="salary" class="form__input-field" />
                         </div>
                     </div>
-
                 </div>
-
                 <input type="submit" value="Ajouter" class="form__submit btn" />
             </form>
         </section>
