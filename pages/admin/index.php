3feca76e8924252c6ad027b5bd9342d29c747f23
sandratra002 <ainarafanomazantsoa@gmail.com>
2024-02-12 15:49:11 +0300

    Changing something

--- pages/admin/index.php
+++ pages/admin/index.php
@@ -27,7 +27,7 @@
                                     <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z"></path>
                                 </svg></div>
                             <form class="text-center" method="post">
-                                <div class="mb-3"><input class="form-control" type="email" name="user_name" placeholder="UserName" style="margin-right: 93px;" value="admin"/></div>
+                                <div class="mb-3"><input class="form-control" type="text" name="user_name" placeholder="UserName" style="margin-right: 93px;" value="admin"/></div>
                                 <div class="mb-3"><input class="form-control" type="password" name="password" placeholder="Password" value="admin"/></div>
                                 <div class="mb-3"><button class="btn btn-primary d-block w-100" type="submit">Login</button></div>
                                 <p class="text-muted">Forgot your password?</p>
