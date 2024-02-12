14473b199d43e5ddb0be84d412833b5323af6955
sandratra002 <ainarafanomazantsoa@gmail.com>
2024-02-12 13:46:42 +0300

    Creation des dossier

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CulThe</title>
+</head>
+<body>
+    <div class="container">
+        <a href="./pages/admin/index.php">Admin</a>
+        <a href="./pages/client/index.php">Client</a>
+    </div>
+</body>
+</html>
