90b76c8e37d5f3d2a967a26ea5a396a546a8332e
sandratra002 <ainarafanomazantsoa@gmail.com>
2024-02-12 14:39:39 +0300

    Adding footer

--- pages/admin/index.php
+++ pages/admin/index.php
@@ -38,6 +38,8 @@
             </div>
         </div>
     </section>
+
+    <?php include("../shared/footer.php"); ?>
 </body>
 
 </html>
---
+++ pages/shared/footer.php
@@ -0,0 +1,22 @@
+<footer class="text-center py-4">
+    <div class="container">
+        <div class="row row-cols-1 row-cols-lg-3">
+            <div class="col">
+                <p class="text-muted my-2">Copyright © 2024 Brand</p>
+            </div>
+            <div class="col">
+                <ul class="list-inline my-2">
+                    <li class="list-inline-item me-4">ETU002468 Sandratra</li>
+                    <li class="list-inline-item me-4">ETU002611 Sanda</li>
+                    <li class="list-inline-item">ETU002593 Mialy</li>
+                </ul>
+            </div>
+            <div class="col">
+                <ul class="list-inline my-2">
+                    <li class="list-inline-item"><a class="link-secondary" href="#">Privacy Policy</a></li>
+                    <li class="list-inline-item"><a class="link-secondary" href="#">Terms of Use</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
