abd891f142aa48d60157d76ef3d9d4be6bba0b78
sandratra002 <ainarafanomazantsoa@gmail.com>
2024-02-12 22:57:00 +0300

    The

---
+++ pages/admin/the.php
@@ -0,0 +1,161 @@
+<div class="body double-col">
+        <section class="left">
+            <h1 class="section__title">Tableau des cultures</h1>
+
+            <hr class="section__sep">
+
+            <div class="section__content">
+                <table>
+                    <tr>
+                        <th class="id number">ID Type</th>
+                        <th class="name">Nom du The</th>
+                        <th class="occupation">Occupation en m<sup>2</sup></th>
+                        <th class="rendement">Rendement en Kg/mois</th>
+                    </tr>
+                    
+                    <tr>
+                        <td class="id number">1</td>
+                        <td class="name">Black tea</td>
+                        <td class="occupation">12</td>
+                        <td class="rendement">30</td>
+                    </tr>
+                    <tr>
+                        <td class="id number">1</td>
+                        <td class="name">Black tea</td>
+                        <td class="occupation">12</td>
+                        <td class="rendement">30</td>
+                    </tr>
+                    <tr>
+                        <td class="id number">1</td>
+                        <td class="name">Black tea</td>
+                        <td class="occupation">12</td>
+                        <td class="rendement">30</td>
+                    </tr>
+                    <tr>
+                        <td class="id number">1</td>
+                        <td class="name">Black tea</td>
+                        <td class="occupation">12</td>
+                        <td class="rendement">30</td>
+                    </tr>
+                    <tr>
+                        <td class="id number">1</td>
+                        <td class="name">Black tea</td>
+                        <td class="occupation">12</td>
+                        <td class="rendement">30</td>
+                    </tr>
+                    <tr>
+                        <td class="id number">1</td>
+                        <td class="name">Black tea</td>
+                        <td class="occupation">12</td>
+                        <td class="rendement">30</td>
+                    </tr>
+                    <tr>
+                        <td class="id number">1</td>
+                        <td class="name">Black tea</td>
+                        <td class="occupation">12</td>
+                        <td class="rendement">30</td>
+                    </tr>
+                    <tr>
+                        <td class="id number">1</td>
+                        <td class="name">Black tea</td>
+                        <td class="occupation">12</td>
+                        <td class="rendement">30</td>
+                    </tr>
+                    <tr>
+                        <td class="id number">1</td>
+                        <td class="name">Black tea</td>
+                        <td class="occupation">12</td>
+                        <td class="rendement">30</td>
+                    </tr>
+                    <tr>
+                        <td class="id number">1</td>
+                        <td class="name">Black tea</td>
+                        <td class="occupation">12</td>
+                        <td class="rendement">30</td>
+                    </tr>
+                    <tr>
+                        <td class="id number">1</td>
+                        <td class="name">Black tea</td>
+                        <td class="occupation">12</td>
+                        <td class="rendement">30</td>
+                    </tr>
+                    <tr>
+                        <td class="id number">1</td>
+                        <td class="name">Black tea</td>
+                        <td class="occupation">12</td>
+                        <td class="rendement">30</td>
+                    </tr>
+                    <tr>
+                        <td class="id number">1</td>
+                        <td class="name">Black tea</td>
+                        <td class="occupation">12</td>
+                        <td class="rendement">30</td>
+                    </tr>
+                    <tr>
+                        <td class="id number">1</td>
+                        <td class="name">Black tea</td>
+                        <td class="occupation">12</td>
+                        <td class="rendement">30</td>
+                    </tr>
+                    <tr>
+                        <td class="id number">1</td>
+                        <td class="name">Black tea</td>
+                        <td class="occupation">12</td>
+                        <td class="rendement">30</td>
+                    </tr>
+                    <tr>
+                        <td class="id number">1</td>
+                        <td class="name">Black tea</td>
+                        <td class="occupation">12</td>
+                        <td class="rendement">30</td>
+                    </tr>
+                    <tr>
+                        <td class="id number">1</td>
+                        <td class="name">Black tea</td>
+                        <td class="occupation">12</td>
+                        <td class="rendement">30</td>
+                    </tr>
+                </table>
+            </div>
+        </section>
+
+        <section class="right">
+            <form action="" method="post" id="login-form" class="form">
+                <h1 class="form__title">Gestion de cultures</h1>
+
+                <hr class="form__sep">
+
+                <div class="form__content">
+                    <div class="form__input vertical">
+                        <label for="tea" class="form__label">Type de The:</label>
+
+                        <select name="tea_id" id="tea" class="form__input-field">
+                            <option value="0">Black tea</option>
+                            <option value="1">Green tea</option>
+                            <option value="2">White tea</option>
+                        </select>
+                    </div>
+
+                    <div class="form__input vertical">
+                        <label for="occupation" class="form__label">Occupation:</label>
+
+                        <div>
+                            <input type="number" name="occupation" id="occupation" class="form__input-field" />
+                            <span class="form__input-field unit">m<sup>2</sup></span>
+                        </div>
+                    </div>
+
+                    <div class="form__input vertical">
+                        <label for="rendement" class="form__label">Rendement:</label>
+
+                        <div>
+                            <input type="number" name="rendement" id="rendement" class="form__input-field" />
+                            <span class="form__input-field unit">Kg/mois</span>
+                        </div>
+                    </div>
+                </div>
+
+                <input type="submit" value="Modifier" class="form__submit btn" />
+            </form>
+        </section>
+    </div>
