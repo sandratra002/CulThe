28119fc0f6d01ded0085cd3ccee904ec8d2b5668
sandratra002 <ainarafanomazantsoa@gmail.com>
2024-02-13 13:10:50 +0300

    This is a test

--- inc/connexion.php
+++ inc/connexion.php
@@ -2,7 +2,7 @@
     function dbconnect(){
         static $connect = null;
         if ($connect===null) {
-            $connect = mysqli_connect('localhost','root','',s'culthe');
+            $connect = mysqli_connect('localhost','root','','culthe');
         }
         return $connect;
     }
