2101a6b9b30e146baa48283eb08c699f7ae0ac00
sandratra002 <ainarafanomazantsoa@gmail.com>
2024-02-13 17:03:19 +0300

    Styling card

--- pages/client/prevision.php
+++ pages/client/prevision.php
@@ -4,8 +4,32 @@
     $cueilleurs = get_all_info_cueilleur();
 ?>
 
+<link rel="stylesheet" href="../../assets/css/card.css">
+
 <div class="body double-col">
     <section class="left">
+        <div class="info__title">
+            <div class="info__parcel info">Montant : 149000Ar</div>
+            <div class="info__tea info">Total : 40000</div>
+        </div>
+        <div class="info__cards">
+            <div class="info__card">
+                <div class="info__parcel info left">###Parcel1</div>
+                <div class="info__tea info center">The Sandratra Tea</div>
+                <div class="info__surface info left">Surface : 12ha</div>
+                <img src="../../assets/images/tea/10.jpg" alt="" class="info__image">
+                <div class="info__surface info left">Pieds : 30</div>
+                <div class="info__surface info left">Poids restant : 300kg</div>
+            </div>
+            <div class="info__card">
+                <div class="info__parcel info">###Parcel1</div>
+                <div class="info__tea info">The Sandratra Tea</div>
+                <div class="info__surface info">Surface : 12ha</div>
+                <img src="../../assets/images/tea/10.jpg" alt="" class="info__image">
+                <div class="info__surface info">Pieds : 30</div>
+                <div class="info__surface info">Poids restant : 300kg</div>
+            </div>
+        </div>
     </section>
 
     <section class="right">
